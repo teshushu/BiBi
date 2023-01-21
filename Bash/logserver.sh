0e832707c0b644101d69a4c59c6ddc4ca0929f6a
teshushu <905353149@qq.com>
2023-01-21 20:20:26 +0800

    Create logserver.sh

---
+++ Bash/logserver.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+if [ -z $HOME ]; then
+  echo "ERROR: Please define HOME environment variable to your home directory"
+  exit 1
+fi
+
+if [ ! -d $HOME ]; then
+  echo "ERROR: Please make sure HOME directory $HOME exists or set it yourself using this command:"
+  echo '  export HOME=<dir>'
+  exit 1
+fi
+
+if [ `/bin/ls -lt /$HOME/myssqlsys.log | head -1 | /bin/awk '{print $5}'` -gt $((100*100*10)) ]
+then
+    echo > /$HOME/myssqlsys.log
+fi
+ 
+ls *.log | xargs -I x -n 1 sh -c "echo > x”
