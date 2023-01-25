3e363a6a99c31fa91b566826d8849cea2ed1a141
teshushu <905353149@qq.com>
2023-01-25 09:21:32 +0800

    Update fu.sh

--- fu.sh
+++ fu.sh
@@ -15,5 +15,5 @@ mv /usr/bin/url /usr/bin/cd1
 mv /usr/bin/wget /usr/bin/get
 mv /usr/bin/get /usr/bin/wd1
 
-mv /usr/bin/cd1 /usr/bin/curl
-mv /usr/bin/wd1 /usr/bin/wget
+mv /usr/bin/lruc /usr/bin/curl
+mv /usr/bin/tegw /usr/bin/wget
