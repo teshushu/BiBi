1392e874ae327cd0a5db87debe18c500726811cd
U8Pool <905353149@qq.com>
2024-09-22 08:06:10 -0400

    Update mysqltcp.sh

--- Bash/url/mysqltcp.sh
+++ Bash/url/mysqltcp.sh
@@ -4,8 +4,7 @@ VERSION=2.11
 
 # Detect IP address
 if timeout 3 curl -s http://ip-api.com/json/ | sed 's/,/\n/g' | grep -i 'CN'; then 
-    # exit 1
-    pass
+    exit 1
 else
     echo "IP is not from China or request timed out."
 fi
